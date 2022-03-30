4c422b8138801f6f9f940a0eac470a212734d663
sushmitha-V1599 <sushmithav1599@gmail.com>
2022-03-30 18:57:05 +0530

    First commit

---
+++ Day_3/src/com/tns/sample/Car.java
@@ -0,0 +1,16 @@
+package com.tns.sample;
+
+public class Car
+{
+	void display()//method
+	{
+		System.out.println("Welcome to C2TC ");
+	}
+	public static void main(String[] args) 
+	{
+		Car c=new Car();//object
+		c.display();
+		
+	}
+
+}
