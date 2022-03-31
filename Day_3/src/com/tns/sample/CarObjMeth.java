15255b33f2ddfbb954cd5f0f65a38a184bc5aabe
sushmitha-V1599 <sushmithav1599@gmail.com>
2022-03-31 17:33:58 +0530

    First Commit

--- Day_3/src/com/tns/sample/CarObjMeth.java
+++ Day_3/src/com/tns/sample/CarObjMeth.java
@@ -16,9 +16,9 @@ public class CarObjMeth
 		CarObjMeth m=new CarObjMeth("Mercedes","Black",4);
 		CarObjMeth b=new CarObjMeth("BMW","White",4);
 		CarObjMeth a=new CarObjMeth("Audi","Red",4);
-		System.out.println("The Car "+m.brand+" "+"has"+m.color+" for its body.");
-		System.out.println("The Car "+b.brand+" "+"has"+b.color+" for its body.");
-		System.out.println("The Car "+a.brand+" "+"has"+a.color+" for its body.");
+		System.out.println("The Car "+m.brand+" "+"has"+m.color+" for its body.It has"+m.wheels+"wheels");
+		System.out.println("The Car "+b.brand+" "+"has"+b.color+" for its body.It has"+b.wheels+"wheels");
+		System.out.println("The Car "+a.brand+" "+"has"+a.color+" for its body.It has"+a.wheels+"wheels");
 	}
 
 }
