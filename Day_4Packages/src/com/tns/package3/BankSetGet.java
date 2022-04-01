1b520b6e73f112130954d0f9cfce6e2185e26412
sushmitha-V1599 <sushmithav1599@gmail.com>
2022-04-01 19:21:58 +0530

    First commit

---
+++ Day_4Packages/src/com/tns/package3/BankSetGet.java
@@ -0,0 +1,22 @@
+package com.tns.package3;
+
+public class BankSetGet 
+{
+	private int balance;
+	
+	public BankSetGet()
+	{
+		
+	}
+	public int getbalance;
+	{
+		System.out.println(balance);
+	}
+
+	public void setbalance(int balance)
+	{
+		System.out.println("your balance is"+balance);
+		
+	}
+
+}
---
+++ Day_4Packages/src/com/tns/package4/EncapsulteSetGet.java
@@ -0,0 +1,18 @@
+package com.tns.package4;
+
+import com.tns.package3.*;
+
+public class EncapsulteSetGet 
+{
+
+
+
+	public static void main(String[] args) 
+	{
+		EncapsulteSetGet bb=new EncapsulteSetGet();
+		bb.setbalance(868688);
+		System.out.println("your balance is"+bb.getbalance);
+
+	}
+
+}
