b2ad9892893c35fc1e4b0337502b723b670a127d
sushmitha-V1599 <sushmithav1599@gmail.com>
2022-04-05 17:45:21 +0530

    first commit

--- Day_4Packages/src/com/tns/package4/EncapsulteSetGet.java
+++ Day_4Packages/src/com/tns/package4/EncapsulteSetGet.java
@@ -1,18 +1,26 @@
 package com.tns.package4;
 
-import com.tns.package3.*;
+//import com.tns.package3.*;
 
 public class EncapsulteSetGet 
 {
 
 
 
+	private static final String getbalance = null;
+
 	public static void main(String[] args) 
 	{
 		EncapsulteSetGet bb=new EncapsulteSetGet();
-		bb.setbalance(868688);
-		System.out.println("your balance is"+bb.getbalance);
+		bb.setbalance();
+		System.out.println("your balance is"+EncapsulteSetGet.getbalance);
 
 	}
 
+	private void setbalance() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
